e8c28a01b90deaee05d1200376cbddea2032a1dd
sholabioku <bilushonline@gmail.com>
2022-03-29 12:38:33 +0100

    Add functionality to  add note for a ticket

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -26,6 +26,34 @@ const getNotes = asyncHandler(async (req, res) => {
   res.status(200).json(notes);
 });
 
+// @desc Create ticket note
+// @route POST /api/tickets/:ticketId/notes
+// @access Private
+const addNote = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.findById(req.params.ticketId);
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not authorized');
+  }
+
+  const note = await Note.create({
+    text: req.body.text,
+    isStaff: false,
+    ticket: req.params.ticketId,
+    user: req.user.id,
+  });
+
+  res.status(200).json(note);
+});
+
 module.exports = {
   getNotes,
+  addNote,
 };
--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,10 +1,10 @@
 // express router for notes
 const router = require('express').Router({ mergeParams: true });
 
-const { getNotes } = require('../controllers/noteController');
+const { getNotes, addNote } = require('../controllers/noteController');
 
 const { protect } = require('../middleware/authMiddleware');
 
-router.route('/').get(protect, getNotes);
+router.route('/').get(protect, getNotes).post(protect, addNote);
 
 module.exports = router;
